79301c7446943df39090c90d35743340a9fbcb59
MihailKur <92600672+MihailKur@users.noreply.github.com>
2021-11-03 13:59:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Harakteristic sob = new Harakteristic("big","Black","Mobs","Bob");
+        sob.getInfo();
+    }
+}
