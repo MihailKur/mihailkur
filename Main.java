e05f97b3e4eb39878e78af2e24423cc8bc3a0426
MihailKur <92600672+MihailKur@users.noreply.github.com>
2021-11-03 14:04:51 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Harakteristic sob = new Harakteristic("big","Black","Mobs","Bob");
-        sob.getInfo();
-    }
-}
